f9a16bbffc4a15f2b5586dac5fde7dd5b118f29d
Adi <adittyapun11@gmail.com>
2025-07-15 16:55:58 +0100

    Create signalR hub

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRChat.Hubs
+{
+    // ChatHub class inherits from SignalR's Hub class.
+    // The Hub class manages connections, groups, and messaging.
+    public class ChatHub: Hub
+    {
+        // This method can be called by a connected client to send a message to all clients
+        public async Task SendMessage(string user,string message)
+        {
+            // SignalR code is asynchronous to provide maximum scalability
+            await Clients.All.SendAsync("Receive message",user,message);
+        }
+    }
+}
