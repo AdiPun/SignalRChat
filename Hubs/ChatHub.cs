d54414ed482456e962b05fb30fec6afdf46d776e
Adi <adittyapun11@gmail.com>
2025-07-15 18:21:52 +0100

    Adds time to each chat log

--- Hubs/ChatHub.cs
+++ Hubs/ChatHub.cs
@@ -7,10 +7,12 @@ namespace SignalRChat.Hubs
     public class ChatHub: Hub
     {
         // This method can be called by a connected client to send a message to all clients
-        public async Task SendMessage(string user,string message)
+        public async Task SendMessage(string user,string message, string time)
         {
             // SignalR code is asynchronous to provide maximum scalability
-            await Clients.All.SendAsync("ReceiveMessage",user,message);
+            await Clients.All.SendAsync("ReceiveMessage",user,message,time);
         }
+
+
     }
 }
--- wwwroot/js/chat.js
+++ wwwroot/js/chat.js
@@ -9,13 +9,14 @@ var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
 //Disable the send button until connection is established.
 document.getElementById("sendButton").disabled = true;
 
-connection.on("ReceiveMessage", function (user, message) {
+connection.on("ReceiveMessage", function (user, message, time) {
     var li = document.createElement("li");
     document.getElementById("messagesList").appendChild(li);
     // We can assign user-supplied strings to an element's textContent because it
-    // is not interpreted as markup. If you're assigning in any other way, you 
+    // is not interpreted as markup. If you're assigning in any other way, you
     // should be aware of possible script injection concerns.
-    li.textContent = `${user} says ${message}`;
+
+    li.textContent = `${time} - ${user}: ${message}`;
 });
 
 connection.start().then(function () {
@@ -27,7 +28,10 @@ connection.start().then(function () {
 document.getElementById("sendButton").addEventListener("click", function (event) {
     var user = document.getElementById("userInput").value;
     var message = document.getElementById("messageInput").value;
-    connection.invoke("SendMessage", user, message).catch(function (err) {
+
+    var time = new Date();
+
+    connection.invoke("SendMessage", user, message, time.toLocaleTimeString()).catch(function (err) {
         return console.error(err.toString());
     });
     event.preventDefault();
